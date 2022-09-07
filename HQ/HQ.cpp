bc2d49879c2416b2756b6e5895e343a2a99b3494
PetrIrik <irinevich@inbox.ru>
2022-09-07 22:32:21 +0500

    update

--- HQ/HQ.cpp
+++ HQ/HQ.cpp
@@ -10,12 +10,13 @@ class Person
 public:
 	Person() = default;
 
-	Person( const std::string &name, const std::string &gander , const size_t &age, const size_t &weight )
+	Person( const std::string &name, const std::string &gander , const size_t age, const size_t weight )
 		: m_name(name), m_gender(gander), m_age(age), m_weight(weight)
 	{
 
 	}
 	std::string getName() const { return m_name; }
+	std::string getGender() const { return m_gender;  }
 	size_t getAge() const { return m_age; }
 	size_t getWeight() const { return m_weight; }
 
@@ -32,36 +33,56 @@ public:
 	Student(const std::string &name, const  std::string &gander,const size_t age,const size_t weight,const size_t yearos)
 		: Person (name, gander, age, weight) , m_StudYear(yearos)
 	{
-		_countC++;
+		_countS++;
 	}
+// yearos - "year of study" - год обучения .
 	~Student()
 	{
-		_countC--;
+		_countS--;
 	}
-	size_t year() 
+	size_t year_increase()// увеличение года 
 	{
 		m_StudYear++;
 	}
-	void SetyearOS(const size_t _year_os)
+
+	void SetyearOS(const size_t _year_os)// Присваивание года 
 	{
 		m_StudYear = _year_os;
 	}
 
-	const int get_countC() const { return _countC; }
 
+	const int get_countC() const { return _countS; }
+	const size_t getStudy_year() const { return m_StudYear; }
 
+	void PrintInfStudent() 
+	{
+		std::cout << " Студент № " << get_countC() << std::endl;
+		std::cout << " Имя = " << getName() << std::endl;
+		std::cout << " Пол = " << getGender() << std::endl;
+		std::cout << " Возраст = " << getAge() << std::endl;
+		std::cout << " Год обучения = " << getStudy_year() << std::endl;
+		std::cout << std::endl;
+	}
 private:
 	size_t m_StudYear;
-	static int _countC;
+	static int _countS;// счетчик студенто 
+	
 };
 
-int Student::_countC = 0;
+int Student::_countS = 0;
 
 
 
 int main (){
 	setlocale(LC_ALL, "rus");
-	Student grisha("Гриша", "Мужчина", 21, 74, 2022);
+	Student grisha("Гриша", "Мужчина", 21, 69, 2022);
+	grisha.PrintInfStudent();
+	
+	Student Petr("Петр", "Мужчина", 24, 75, 2022);
+	Petr.PrintInfStudent();
+
+	Student Anna("Анна", " Женщина ", 20, 59, 2022);
+	Anna.PrintInfStudent();
 	
 	return 0;
 }
