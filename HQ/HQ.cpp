5556dab17720cc7f29d7c834b095c51537a6e868
PetrIrik <irinevich@inbox.ru>
2022-09-07 21:36:00 +0500

    refresh file

--- HQ/HQ.cpp
+++ HQ/HQ.cpp
@@ -1,148 +1,67 @@
 ﻿#include <iostream>
-#include <cmath>
-#include <cstdint>
+#include <string>
+/*1. Создать класс Person (человек) с полями: имя, возраст, пол и вес. 
+Определить методы переназначения имени, изменения возраста и веса. Создать производный класс Student (студент), имеющий поле года обучения. 
+Определить методы переназначения и увеличения этого значения.
+Создать счетчик количества созданных студентов. В функции main() создать несколько (не больше трёх) студентов. Вывести информацию о них.*/
 
-class Power 
+class Person
 {
 public:
-	void Set(const float a, const float b)
-	{
-		_a = a;
-		_b = b;
-	}
+	Person() = default;
 
-	void Calculete() 
+	Person( const std::string &name, const std::string &gander , const size_t &age, const size_t &weight )
+		: m_name(name), m_gender(gander), m_age(age), m_weight(weight)
 	{
-		std::cout <<"Число :" << _a <<" в степени :"<< _b << " = " << pow(_a, _b) << std::endl;
+
 	}
+	std::string getName() const { return m_name; }
+	size_t getAge() const { return m_age; }
+	size_t getWeight() const { return m_weight; }
+
+private:
+	std::string m_name;
+	std::string m_gender;
+	size_t m_age;
+	size_t m_weight;
+};
+class Student:Person
+{
+public:
 	
+	Student(const std::string &name, const  std::string &gander,const size_t age,const size_t weight,const size_t yearos)
+		: Person (name, gander, age, weight) , m_StudYear(yearos)
+	{
+		_countC++;
+	}
+	~Student()
+	{
+		_countC--;
+	}
+	size_t year() 
+	{
+		m_StudYear++;
+	}
+	void SetyearOS(const size_t _year_os)
+	{
+		m_StudYear = _year_os;
+	}
+
+	const int get_countC() const { return _countC; }
+
 
 private:
-	float _a;
-	float _b;
-};
-//2. Написать класс с именем RGBA, который содержит 4 переменные-члена типа std::uint8_t: m_red, m_green, m_blue и m_alpha (#include cstdint для доступа к этому типу). 
-// Задать 0 в качестве значения по умолчанию для m_red, m_green, m_blue и 255 для m_alpha. Создать конструктор со списком инициализации членов, 
-//который позволит пользователю передавать значения для m_red, m_blue, m_green и m_alpha. Написать функцию print(), которая будет выводить значения переменных-членов.
-
-class RGBA
-{
-public:
-
-	RGBA(uint8_t red = 0 , uint8_t green = 0 , uint8_t blue= 0, uint8_t alpha= 255) :m_red(red),m_green(green), m_blue(blue), m_alpha(alpha)
-	{
-
-	}
-
-	void print()
-	{
-		std::cout << " R " << static_cast<int>(m_red) << std::endl;
-		std::cout << " G " << static_cast<int>(m_green) << std::endl;
-		std::cout << " B " << static_cast<int>(m_blue) << std::endl;
-		std::cout << " A " << static_cast<int>(m_alpha) << std::endl;
-
-	}
-
-private:
-	uint8_t m_red;
-	uint8_t m_green;
-	uint8_t m_blue;
-	uint8_t m_alpha;
+	size_t m_StudYear;
+	static int _countC;
 };
 
-/*3 .Написать класс, который реализует функциональность стека. Класс Stack должен иметь:
-• private-массив целых чисел длиной 10;
+int Student::_countC = 0;
 
-• private целочисленное значение для отслеживания длины стека;
-
-• public-метод с именем reset(), который будет сбрасывать длину и все значения элементов на 0;
-
-• public-метод с именем push(), который будет добавлять значение в стек. push() должен возвращать значение false, если массив уже заполнен, и true в противном случае;
-
-• public-метод с именем pop() для вытягивания и возврата значения из стека. Если в стеке нет значений, то должно выводиться предупреждение;
-
-• public-метод с именем print(), который будет выводить все значения стека.*/
-class Stack 
-{
-public:
-	void reset()
-	{
-		for (size_t i = 0; i < size; i++)
-		{
-			m_Arry[i] = 0;
-		}
-		elem = 0;
-	}
-	bool push(int out)
-	{
-		if (elem < size)
-		{
-			m_Arry[elem] = out;
-			elem++;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	int pop()
-	{
-		if (elem > 0 )
-		{
-			elem--;
-			int element = m_Arry[elem];
-				return element;
-		}
-		else
-		{
-			std::cout << " Нет значений в массиве " << std::endl;
-			return 0;
-		}
-	}
-	void print()
-	{
-		std::cout << " ( ";
-		for (size_t i = 0; i < elem; i++)
-		{
-			std::cout << " " << m_Arry[i];
-		}
-		std::cout << " ) " << std::endl;
-	}
-
-private:
-	const static size_t size{ 10 };
-	int m_Arry[size]{};
-	size_t elem{ 0 };
-};
 
 
 int main (){
 	setlocale(LC_ALL, "rus");
-	std::cout << "Первое задание :" << std::endl;
-	Power task;
-	task.Set(5, 5);
-	task.Calculete();
-	std::cout << std::endl;
-	std::cout << "Второе задание :" << std::endl;
-	RGBA task2(1, 3, 4, 8);
-	task2.print();
-	std::cout << "Третие  задание :" << std::endl;
-	Stack stack;
-	stack.reset();
-	stack.print();
-
-	stack.push(3);
-	stack.push(7);
-	stack.push(5);
-	stack.print();
-
-	stack.pop();
-	stack.print();
-
-	stack.pop();
-	stack.pop();
-	stack.print();
-
+	Student grisha("Гриша", "Мужчина", 21, 74, 2022);
+	
 	return 0;
 }
